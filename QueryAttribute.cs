cbc2bede1c95799cb0b4589e4e29bd69365cb78f
Blake Connally <blake@blakecodes.com>
2019-01-11 18:04:32 -0500

    Initial version commit

---
+++ QueryAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modsi.QueryAttribute
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class QueryAttribute : Attribute
+    {
+        public string Name;
+        public string NullValue;
+        public bool Required;
+
+        public QueryAttribute(string Name, bool Required, string NullValue = null)
+        {
+            this.NullValue = NullValue;
+            this.Required = Required;
+            this.Name = Name;
+        }
+    }
+}
---
+++ QueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Modsi.QueryAttribute
+{
+    public static class QueryHandler
+    {
+        public static string BuildQuery<T>(T request)
+        {
+            Type type = request.GetType();
+            PropertyInfo[] properties = type.GetProperties();
+            Dictionary<string, string> queryParameters = new Dictionary<string, string>();
+            string queryString = "?";
+
+            foreach (PropertyInfo property in properties)
+            {
+                QueryAttribute queryAtrributes = property.GetCustomAttribute<QueryAttribute>();
+
+                if (queryAtrributes != null)
+                {
+                    string name = property.Name;
+                    string value = property.GetValue(request)?.ToString();
+
+                    if (value != null && value != queryAtrributes.NullValue)
+                    {
+                        queryParameters.Add(queryAtrributes.Name, value);
+                    }
+                }
+            }
+
+            foreach (var param in queryParameters)
+            {
+                queryString += param.Key + "=" + param.Value + "&";
+            }
+
+            queryString = queryString.Remove(queryString.Length - 1, 1);
+
+            return queryString;
+        }
+    }
+}
